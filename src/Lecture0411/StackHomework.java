c91fec39c16da778f0281d1be9de877f944e63e9
AlexanderKozhuharov <sashko896@mail.bg>
2021-11-05 18:11:22 +0200

    Homework - 04.11

--- src/Lecture0411/StackHomework.java
+++ src/Lecture0411/StackHomework.java
@@ -3,7 +3,6 @@ package Lecture0411;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-
 public class StackHomework {
     public static void main(String[] args) {
         ArrayList<Integer> stack = new ArrayList<>();
@@ -11,27 +10,37 @@ public class StackHomework {
         System.out.println("Please enter your desired initial stack size: ");
         int size = input.nextInt();
         for (int i = 0; i < size; i++) {
-            System.out.println("Please fill the stack: ");
-            stack.add(input.nextInt());
+            if (size - i == 1) {
+                System.out.println(String.format("Please fill the stack with %s more number: ", size - i));
+                stack.add(input.nextInt());
+            } else {
+                System.out.println(String.format("Please fill the stack with %s more numbers: ", size - i));
+                stack.add(input.nextInt());
+            }
         }
+        StackHomework.pop(stack);
         StackHomework.push(stack, 100);
         StackHomework.push(stack, 101);
         System.out.println(stack);
         StackHomework.pop(stack);
         System.out.println(stack);
-        StackHomework.top(stack);
+        StackHomework.peak(stack);
     }
 
     static void push(ArrayList<Integer> arr, int nextNumber) {
-
-        arr.add(arr.size(), nextNumber);
+        arr.add(nextNumber);
     }
 
     static void pop(ArrayList<Integer> arr) {
-        arr.remove(arr.size() - 1);
+        if (arr.isEmpty()){
+            System.out.println("The stack is empty.");
+        }
+        else{
+            arr.remove(arr.size() - 1);
+        }
     }
 
-    static void top(ArrayList<Integer> arr) {
+    static void peak(ArrayList<Integer> arr) {
         System.out.println("The top element is: " + arr.get(arr.size() - 1));
     }
 }
